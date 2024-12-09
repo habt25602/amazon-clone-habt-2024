460895f44194db54ed03317d4d07629de5a4fd4d
Habtamu Tilahun <habt256@gmail.com>
2024-12-09 22:49:41 +0000

    deploying to netlify

--- Amazon-clone-project/src/Api/axios.js
+++ Amazon-clone-project/src/Api/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://127.0.0.1:5001/clone-d2915/us-central1/api",
+  // baseURL: "http://127.0.0.1:5001/clone-d2915/us-central1/api",
+  baseURL: "https://us-central1-clone-d2915.cloudfunctions.net/api",
 });
 
 export { axiosInstance };
